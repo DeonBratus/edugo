b1d01a89ea68a086baca6ad68b5d5ba56472a9b2
bratusDA <bratus26d@gmail.com>
2025-05-18 19:26:59 +0000

    clean

--- python_code/exmaple.py
+++ python_code/exmaple.py
@@ -1,39 +1,14 @@
 import requests
 import json
 
-body0 = [
-    { "type": "calc", "op": "+", "var": "x", "left": 1, "right": 2 },
-    { "type": "print", "var": "x" }
-]
+# Чтение данных из JSON-файла
+with open('data.json', 'r') as file:
+    requests_data = json.load(file)
 
-body1 = [
-    { "type": "calc", "op": "+", "var": "x", "left": 10, "right": 2 },
-    { "type": "print", "var": "x" },
-    { "type": "calc", "op": "-", "var": "y", "left": "x", "right": 3 },
-    { "type": "calc", "op": "*", "var": "z", "left": "x", "right": "y" },
-    { "type": "print", "var": "w" },
-    { "type": "calc", "op": "*", "var": "w", "left": "z", "right": 0 }
-]
+# URL сервиса
+service_url = "http://127.0.0.1:8000/"
 
-body2 = [
-    { "type": "calc", "op": "+", "var": "x", "left": 10, "right": 2 },
-    { "type": "calc", "op": "*", "var": "y", "left": "x", "right": 5 },
-    { "type": "calc", "op": "-", "var": "q", "left": "y", "right": 20 },
-    { "type": "calc", "op": "+", "var": "unusedA", "left": "y", "right": 100 },
-    { "type": "calc", "op": "*", "var": "unusedB", "left": "unusedA", "right": 2 },
-    { "type": "print", "var": "q" },
-    { "type": "calc", "op": "-", "var": "z", "left": "x", "right": 15 },
-    { "type": "print", "var": "z" },
-    { "type": "calc", "op": "+", "var": "ignoreC", "left": "z", "right": "y" },
-    { "type": "print", "var": "x" }
-]
-
-
-response = requests.post(url="http://127.0.0.1:8000/", json=body0)
-print(f"Response 1 - {response.json()}")
-
-response = requests.post(url="http://127.0.0.1:8000/", json=body1)
-print(f"Response 2 - {response.json()}")
-
-response = requests.post(url="http://127.0.0.1:8000/", json=body2)
-print(f"Response 3 - {response.json()}")
+# Отправка каждого запроса
+for request in requests_data['requests']:
+    response = requests.post(url=service_url, json=request['body'])
+    print(f"Response for {request['name']} - {response.json()}")
